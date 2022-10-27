421a796f832631e9463279523e094426c1a544f3
yongnology <cassius20@naver.com>
2022-10-28 01:49:09 +0900

    2022.10.27.(목) 8.2 서블릿의 여러가지 포워드 방법(redirect, Refresh, loaction, dispatcher)

---
+++ pro08/src/main/java/sec02/ex02/FirstServlet.java
@@ -0,0 +1,33 @@
+// dispatch를 이용한 forward 방법
+
+package sec02.ex02;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/first4")
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		
+		RequestDispatcher dispatch = request.getRequestDispatcher("second4");
+		dispatch.forward(request, response);
+	}
+
+}
---
+++ pro08/src/main/java/sec02/ex02/SecondServlet.java
@@ -0,0 +1,35 @@
+// dispatch를 이용한 forward 방법
+
+package sec02.ex02;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/second4")
+public class SecondServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		out.print("<html><body>");
+		out.print("dispatch를 이용한 forward 실습");
+		out.print("</body></html>");
+		
+	}
+
+}
