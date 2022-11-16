25404f9d4cbce5e7ecfe89020cd0f982083150f2
yongnology <cassius20@naver.com>
2022-11-16 22:58:42 +0900

    9.5 encodeURL() 사용법

---
+++ pro09/src/main/java/sec04/ex01/SesionTest5.java
@@ -0,0 +1,51 @@
+package sec04.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/login4")
+public class SesionTest5 extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doHandle(request, response);
+	}
+	private void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		HttpSession session = request.getSession();
+		String user_id = request.getParameter("user_id");
+		String user_pw = request.getParameter("user_pw");
+		if(session.isNew()) {
+			if(user_id != null) {
+				session.setAttribute("user_id", user_id);
+				String url= response.encodeUrl("login4");
+				out.print("<a href='"+url+"'>로그인 상태 확인</a>");
+			} else {
+				out.print("<a href='login4.html'>다시 로그인하세요!!</a>");
+				session.invalidate();
+			}
+		} else {
+			user_id = (String) session.getAttribute("user_id");
+			if(user_id != null && user_id.length() != 0) {
+				out.print("안녕하세요" + user_id + "님!!");
+			} else {
+				out.print("<a href='login4.html'>다시 로그인하세요!</a>");
+				session.invalidate();
+			}
+		}
+	}
+
+}
