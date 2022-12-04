325e5adcb6e5035295342eedf5a8a9d7947ab5dd
yongnology <cassius20@naver.com>
2022-12-05 01:02:42 +0900

    10.3 필터 사용하기

--- pro10/src/main/java/sec02/ex01/TestServlet3.java
+++ pro10/src/main/java/sec02/ex01/TestServlet3.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 // @WebServlet("*.do")
-@WebServlet("/*")
+// @WebServlet("/*")
 public class TestServlet3 extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
---
+++ pro10/src/main/java/sec03/ex01/EncodingFilter.java
@@ -0,0 +1,45 @@
+package sec03.ex01;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Servlet Filter implementation class EncodingFilter
+ */
+@WebFilter("/*")
+public class EncodingFilter extends HttpFilter implements Filter {
+       
+    public EncodingFilter() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		System.out.println("doFilter 호출");
+		request.setCharacterEncoding("utf-8");
+		
+		String context = ((HttpServletRequest) request).getContextPath();
+		String pathinfo = ((HttpServletRequest) request).getRequestURI();
+		String realPath = request.getRealPath(pathinfo);
+		String mesg = " Contet 정보 : "+context + "\n URI 정보: " + pathinfo + "\n 물리적 경로 : " + realPath;
+		System.out.println(mesg);
+		
+		long begin = System.currentTimeMillis();
+		chain.doFilter(request, response);
+		
+		long end = System.currentTimeMillis();
+		System.out.println("작업시간 : "+ (end-begin) + "ms");
+	}
+
+}
