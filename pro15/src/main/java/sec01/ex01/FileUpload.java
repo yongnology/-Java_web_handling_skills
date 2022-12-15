65e0e973d03378a6dbb25d16f5d11b05838e2352
yongnology <cassius20@naver.com>
2022-12-15 11:27:50 +0900

    16장 HTML5와 제이쿼리 16.1 HTML5의 주요 개념 16.2 HTML5 시맨틱 웹을 위한 구성 요소

--- pro15/src/main/java/sec01/ex01/FileUpload.java
+++ pro15/src/main/java/sec01/ex01/FileUpload.java
@@ -11,7 +11,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
